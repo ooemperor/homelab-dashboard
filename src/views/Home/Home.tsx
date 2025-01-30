cd9e8326e9b4650529cf75f274375435ed89b748
oo_emperor <michael.kaiser@emplabs.ch>
2025-01-30 20:45:23 +0100

    building basic nav into the home page

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -11,6 +11,22 @@ export default function Home() {
                 <div className="col">
                     <h1>Home</h1>
                 </div>
+
+                <div className="row">
+                    <div className="col">
+                        <div className="card text-center">
+                            <div className="card-header">
+                                Proxmox
+                            </div>
+                            <div className="card-body">
+                                <p>
+                                    <img src="https://proxmox.com/images/proxmox/Proxmox_logo_standard_hex_400px.png#joomlaImage://local-images/proxmox/Proxmox_logo_standard_hex_400px.png?width=400&amp;height=60"/>
+                                </p>
+                                <a href="/proxmox/nodes" className="btn btn-primary">Nodes</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/views/Proxmox/ProxmoxHome.tsx
+++ src/views/Proxmox/ProxmoxHome.tsx
@@ -3,8 +3,6 @@
  * @author ooemperor
  */
 import React from "react";
-import {Toast} from "../../components/Toast";
-import {toastService} from "../../services/ToastService";
 
 
 export default function ProxmoxHome() {
@@ -14,6 +12,45 @@ export default function ProxmoxHome() {
                 <div className="col">
                     <h1>Proxmox</h1>
                 </div>
+            </div>
+
+            <div className="row">
+                <div className="col">
+                    <div className="card text-center">
+                        <div className="card-header">
+                            Nodes
+                        </div>
+                        <div className="card-body">
+                            <p className="card-text">Overview over all the cluster Nodes</p>
+                            <a href="/proxmox/nodes" className="btn btn-primary">Nodes</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col">
+                    <div className="card text-center">
+                        <div className="card-header">
+                            LXCs
+                        </div>
+                        <div className="card-body">
+                            <p className="card-text">Overview over all the LXCs</p>
+                            <a href="/proxmox/lxc" className="btn btn-primary">LXCs</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col">
+                    <div className="card text-center">
+                        <div className="card-header">
+                            VMs
+                        </div>
+                        <div className="card-body">
+                            <p className="card-text">Overview over all the VMs</p>
+                            <a href="/proxmox/vm" className="btn btn-primary">VMs</a>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     )
